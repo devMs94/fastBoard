31c7c855e42baef558dc384caf5ab1e251c2eaf0
yuminsang <nbghza@gmail.com>
2023-02-01 16:19:40 +0900

    #15 - AuditingFields 추출
    생성자,생성일시,수정자,수정일시는 반복적으로 
    엔티티 클래스에 들어가는 요소이므로
    도메인괴 직접 연관이 없는 요소이므로 추출이 가능
    
    `@MappedSuperclass` 이용하여 상속 방식으로 추출함 
    
    그외 `@DateTimeFormat` 요소 추가및 옵션 추가

--- src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -3,14 +3,10 @@ package com.fastcampus.projectboard.domain;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
+
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Getter
@@ -22,21 +18,21 @@ import java.util.Objects;
 })
 @EntityListeners(AuditingEntityListener.class)
 @Entity
-public class ArticleComment {
+public class ArticleComment extends AuditingFields {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Setter @ManyToOne(optional = false) private Article article; // 게시글(ID)
-    @Setter @Column(nullable = false, length = 500) private String content; // 본문
+    @Setter
+    @ManyToOne(optional = false)
+    private Article article; // 게시글(ID)
+    @Setter
+    @Column(nullable = false, length = 500)
+    private String content; // 본문
 
-    // metadata
-    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
-    @CreatedBy @Column(nullable = false, length = 100) private String createdBy;   // 생성자
-    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일시
-    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; // 수정자
 
-    protected ArticleComment() {}
+    protected ArticleComment() {
+    }
 
     private ArticleComment(Article article, String content) {
         this.article = article;
