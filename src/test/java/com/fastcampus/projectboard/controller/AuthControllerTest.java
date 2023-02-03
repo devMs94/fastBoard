8d0b4aad99d4adbe279b55ec2b0cd0d8391d2afc
yuminsang <nbghza@gmail.com>
2023-02-03 16:10:35 +0900

    # 19 - 로그인 컨트롤러 테스트 + 시큐리티 설정을 컨트롤러 테스트에 반영
    스프링 시큐리티가 자동 생성해준 `/login` 컨트롤러 테스트, 
    그리고 시큐리티 설정을 부를 수 있도록 `@import` 추가

---
+++ src/test/java/com/fastcampus/projectboard/controller/AuthControllerTest.java
@@ -0,0 +1,43 @@
+package com.fastcampus.projectboard.controller;
+
+import com.fastcampus.projectboard.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
+@DisplayName("View 컨트롤러 - 인증")
+@WebMvcTest
+@Import(SecurityConfig.class)
+public class AuthControllerTest {
+
+
+    private final MockMvc mvc;
+
+    public AuthControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] 로그인 페이지 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenTryingToLogin_thenReturnsLoginPage() throws Exception {
+        // Given
+
+        // When
+
+        // Then
+        mvc.perform(MockMvcRequestBuilders.get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+
+    }
+
+
+}
