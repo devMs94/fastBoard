e1c10d53a5a08a16477391627feaa9594017cdbd
yuminsang <nbghza@gmail.com>
2023-02-05 14:32:52 +0900

    #19 - 헤더, 푸터에 부트스트랩 예제 적용
    부트스트랩 example 을 활용해서 디자인을 입힘 
    
    * https://getbootstrap.com/docs/5.2/examples/headers/
    
    * https://getbootstrap.com/docs/5.2/examples/footers/

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
 -- 테스트 계정
--- TODO: 테스트용이지만 비밀번호가 노출된 데이터 세팅. 개선하는 것이 좋을 지 고민해 보자.
-insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
-    ('minsang', 'minsasng', 'minsang', 'minsang@mail.com', 'I am minsang.', now(), 'minsang', now(), 'minsang')
-;
-
+-- -- TODO: 테스트용이지만 비밀번호가 노출된 데이터 세팅. 개선하는 것이 좋을 지 고민해 보자.
+-- insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
+--     ('minsang', 'minsasng', 'minsang', 'minsang@mail.com', 'I am minsang.', now(), 'minsang', now(), 'minsang')
+-- ;
+--
 
 
 
